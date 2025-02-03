b022302d6e29f352c5b928ad54d7f071cfba6ee6
LuquitaDoidao <lucasamarantelima7@gmail.com>
2025-02-03 17:24:34 -0300

    aaa

--- assets/js/codigos.js
+++ assets/js/codigos.js
@@ -1,3 +1,4 @@
+// mudança de paginas
 function selecionaAlbum(id) {
   // Ocultar todas as sections de playlists
   document.querySelectorAll('.conteudo').forEach((section) => {
@@ -11,3 +12,16 @@ function selecionaAlbum(id) {
   const section = document.getElementById(`conteudo-${id}`);
   section.style.display = 'block';
 }
+
+// enviar formulario
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".formulario form");
+  const tampa = document.querySelector(".tampa");
+  const botao = form.querySelector("button"); // Supondo que o botão de envio seja um <button> dentro do form
+
+  botao.addEventListener("click", function (event) {
+      event.preventDefault(); // Evita o envio do formulário, caso necessário
+      form.style.display = "none"; // Esconde o formulário
+      tampa.style.display = "flex"; // Mostra o elemento com a classe "tampa"
+  });
+});
